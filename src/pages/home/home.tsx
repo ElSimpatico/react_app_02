c77a6cfb13415e7b8ead25377096f25e99e7ea39
Aaron Velasco <tiquismiquisss@gmail.com>
2020-03-08 13:22:15 +0100

    feat(Home Page): Add constants folder
    Add NAV_TABS constants

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,22 +1,14 @@
 import React, { ReactElement } from 'react';
 
 import { Grid } from '@material-ui/core';
-import { NavigationTab } from '../../components';
-
-const TabsMock = [
-    {
-        title: 'seat'
-    },
-    {
-        title: 'FAQs'
-    }
-];
+import { NavigationTabs } from '../../components';
+import { NAV_TABS } from './constants/nav-tabs';
 
 export function Home(): ReactElement {
     return (
         <Grid container spacing={0}>
             <Grid item xs={12} sm={6}>
-                <NavigationTab tabs={TabsMock} />
+                <NavigationTabs navTabs={NAV_TABS} />
             </Grid>
             <Grid item xs={12} sm={6}></Grid>
         </Grid>
