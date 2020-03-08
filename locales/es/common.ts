9a7c59461a55cf810457d96987804688d5a937f2
Aaron Velasco <tiquismiquisss@gmail.com>
2020-03-08 13:20:02 +0100

    feat(Locales): Add title home navs

--- locales/es/common.ts
+++ locales/es/common.ts
@@ -1,3 +1,5 @@
 export const common_es = {
-    'welcome-react': 'Welcome to React and react-i18next'
+    'welcome-react': 'Welcome to React and react-i18next',
+    seat: 'Seat',
+    faqs: 'FAQs'
 };
