a84f6a3cf50b9de12e2e106d51e41b5e86059738
Aaron Velasco <tiquismiquisss@gmail.com>
2020-03-11 22:12:49 +0100

    feat(Locales): Add contacts

--- locales/en/common.ts
+++ locales/en/common.ts
@@ -1,5 +1,6 @@
 export const common_en = {
     'welcome-react': 'Welcome to React and react-i18next',
     seat: 'Seat',
-    faqs: 'FAQs'
+    faqs: 'FAQs',
+    contacts: 'Contacts'
 };
--- locales/es/common.ts
+++ locales/es/common.ts
@@ -1,5 +1,6 @@
 export const common_es = {
     'welcome-react': 'Welcome to React and react-i18next',
     seat: 'Seat',
-    faqs: 'FAQs'
+    faqs: 'FAQs',
+    contacts: 'Contactos'
 };
